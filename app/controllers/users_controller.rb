0c218c94be664c2cfc30a0bdad73d91fd0edc4f8
Evan B <konros@yahoo.com>
2020-06-08 12:07:37 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if !@user.valid?
-            redirect_to signup_path, flash:{notice: "Required input missing, or account with that username and/or email already exists. Be sure password has at least 7 characters and that password fields match." }
+            redirect_to signup_path, flash:{notice: "Required input missing, or account with that username and/or email may already exist. Be sure password has at least 7 characters and that password fields match." }
         elsif @user.valid? && @user.save
             session[:user_id] = @user.id
             redirect_to @user, flash:{notice:"Account created. Thank you for joining Tarheel Teachers!"}

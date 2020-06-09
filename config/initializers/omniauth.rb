30d2899595299345fe7e74f863d236e0e33a3d7a
Evan B <konros@yahoo.com>
2020-06-08 21:06:56 -0400

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], provider_ignores_state: true
     provider :developer unless Rails.env.production?
 end

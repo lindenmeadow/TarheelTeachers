49be9cd2cc61c8150e80167817d60e4fb65e6d7f
Evan B <konros@yahoo.com>
2020-06-14 17:38:28 -0400

    Update forum.rb

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,6 +1,7 @@
 class Forum < ApplicationRecord
     has_many :discussions, dependent: :destroy
     has_many :users, through: :discussions
+    has_many :comments, through: :discussions
     validates :forum_name, presence: true
     scope :discussions, -> {where('forum_id = ?', @forum.id) } 
     resourcify

1dd7e6fde16e63818cbb0688a7641878da5b9ee0
Evan B <konros@yahoo.com>
2020-06-24 17:33:10 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users, :forums, :comments, :discussions
+  
+  resources :discussions, only: [:show] do
+    resources :comments, only: [:index, :new]
+  end
  
   root to: "home#index"
 

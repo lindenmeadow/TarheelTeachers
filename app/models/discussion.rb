1a9647ab13551b91fd37c61d9bfce5c8fa37b39f
Evan B <konros@yahoo.com>
2020-06-29 20:00:57 -0400

    Update discussion.rb

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,6 +2,8 @@ class Discussion < ApplicationRecord
     belongs_to :forum
     belongs_to :user
     has_many :comments, dependent: :destroy
+    
+    scope :recent, -> { order(created_at: :desc) }
 
     validates :title, :content, presence: true
     resourcify
